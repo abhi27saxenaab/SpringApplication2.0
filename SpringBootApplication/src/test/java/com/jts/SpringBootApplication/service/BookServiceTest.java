fc20f34cc928c23e7fcb3db2a64b3fbb7b7b2712
abhi27saxena.ab <abhi27saxena.ab@gmail.com>
2025-08-03 18:10:56 -0700

    CURD operation for book and author

---
+++ SpringBootApplication/src/test/java/com/jts/SpringBootApplication/service/BookServiceTest.java
@@ -0,0 +1,58 @@
+package com.jts.SpringBootApplication.service;
+
+import com.BookStore.dto.BookRequestDTO;
+import com.BookStore.entity.Book;
+import com.BookStore.repository.BookRepository;
+import com.BookStore.service.impl.BookService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock
+    private BookRepository bookRepository; //
+
+    @Test
+    void saveBookShouldAddSuccessfully(){
+        BookRequestDTO bookdto = new BookRequestDTO();
+        bookdto.setAuthorId(1);
+        bookdto.setTitle("test");
+        bookdto.setIsbn("111");
+
+        Book expectedBook = new Book();
+        expectedBook.setBookId(1);
+        expectedBook.setTitle("test");
+        expectedBook.setIsbn("111");
+        expectedBook.setAuthorId(1);
+
+        // Define mock behavior
+        when(bookRepository.save(any(Book.class))).thenReturn(expectedBook);
+
+
+
+        Book actualBook = bookService.saveBook(bookdto);
+
+        assertNotNull(actualBook);
+        assertEquals("test", actualBook.getTitle());
+        assertEquals("111", actualBook.getIsbn());
+        assertEquals(1, actualBook.getAuthorId());
+
+
+
+
+    }
+}
